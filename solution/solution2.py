7b5de61c3f5be582c74181b84ccd6aec8e5170a4
Beyza Akdemir <byzakdmrx@hotmail.com>
2023-01-14 21:15:32 +0300

    çözümler eklendi

---
+++ solution/solution2.py
@@ -0,0 +1,12 @@
+#ikinci sorunun çözümü
+
+list1 = [[1, 2], [3, 4], [5, 6, 7]]  
+
+def reverse_list(l):
+    l.reverse()
+    for sublist in l:
+        if type(sublist) == list:
+            reverse_list(sublist)
+
+reverse_list(list1)
+print(list1)
